dd4406122303e12c264e12e21e1e9f9d1b65df50
Dinamiet <iceman.sm39@gmail.com>
2023-03-05 10:21:43 +0200

    TCP connection and data dump

--- main.go
+++ main.go
@@ -1,7 +1,39 @@
 package main
 
-import "fmt"
+import (
+	"encoding/hex"
+	"net"
+	"os"
+)
+
+const (
+	HOST = "mico.lan"
+	PORT = "8000"
+	TYPE = "tcp"
+)
 
 func main() {
-	fmt.Println("Hello, World!")
+	tcpServer, err := net.ResolveTCPAddr(TYPE, HOST+":"+PORT)
+	if err != nil {
+		println("Resolve Failed:", err.Error())
+		os.Exit(1)
+	}
+
+	conn, err := net.DialTCP(TYPE, nil, tcpServer)
+	if err != nil {
+		println("Dail failed:", err.Error())
+		os.Exit(2)
+	}
+
+	received := make([]byte, 1024)
+	for {
+
+		numRead, err := conn.Read(received)
+		if err != nil {
+			println("Read data failed:", err.Error())
+			os.Exit(3)
+		}
+		data := hex.EncodeToString(received[:numRead])
+		println("%d -> %s\n", numRead, data)
+	}
 }
